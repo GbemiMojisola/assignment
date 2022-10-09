4fcc08f82569b92b1fcd0b29681cdda11504a441
user.email <laragbemi@gmail.com>
2022-10-09 10:39:39 +0100

    assignment

---
+++ myclass.js
@@ -0,0 +1,61 @@
+// import React, { useState } from "react";
+// import "./App.css";
+
+// const App = () => {
+
+// 	const [counter, setCounter] = useState(0);
+
+// 	let Quote = [
+// 		"This is the day the lord has made",
+//         "I am Unstoppable",
+// 		"It's a New Day",
+//         "Nothing is Impossible"
+//     ]
+// 	const decrement = () => {
+
+// 		setCounter(counter - 1);
+// 	];
+// 	};
+
+// 	console.log(counter);
+// 	console.log(344 / 3);
+
+// 	const increment = () => {
+// 	return (
+// 		setCounter(counter + 1);
+// 		<div className='Container'>
+// 	};
+// 			<div>{counter} </div>
+
+// 			<div>{counter % 4} </div>
+// 	const decrement = () => {
+// 			<div>{Quote[counter % 4]} </div>
+// 		setCounter(counter - 1);
+// 			<div className='wrapper'>
+// 	};
+// 				<button onClick={decrement}> -</button>
+
+// 				<button onClick={increment}>+ </button>
+// 	console.log(344 / 3);
+// 			</div>
+
+// 		</div>
+// 	return (
+// 	);
+// 		<div className='Container'>
+// };
+// 			<div>{counter} </div>
+
+// 			<div>{counter % 4} </div>
+// export default App;
+// 			<div>{Quote[counter % 4]} </div>
+
+// 			<div className='wrapper'>
+// 				<button onClick={decrement}> -</button>
+// 				<button onClick={increment}>+ </button>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default App;
